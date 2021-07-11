a8c16e8b39295b4ecb6633e7f05ef729c1852565
Mikey Epps <55078570+meppps@users.noreply.github.com>
2021-07-11 13:01:17 -0700

    code cleanup

--- subtitles.js
+++ subtitles.js
@@ -1,11 +1,13 @@
-// document.querySelectorAll('.vjs-text-track-display')[1].innerText
+
+// Create transcripts of talks from subtitles
+
 var prevText;
 var wordsList = [];
 var massList = [];
 var masstext;
-var literallyEverything;
 
-// Create btns
+
+// Create file button
 var createBtn = document.createElement('button');
 createBtn.id = 'create';
 createBtn.type = 'button';
@@ -13,34 +15,30 @@ createBtn.innerHTML = '<span class="buttonText">Create File</span>';
 createBtn.classList.add('dropdownHandle')
 document.querySelector('.mediaCitation').insertAdjacentElement('beforebegin',createBtn);
 
-// Download btn
+
+// Download file button
 var download = document.createElement('a');
 download.id = 'downloadlink';
-// download.type = 'span';
-// download.download='info.txt';
 download.download = `transcript_${Date.now()}`;
 download.innerText = '  Download File';
-
-
 document.querySelector('.mediaCitation').insertAdjacentElement('beforebegin',download);
 
 
-
+// Check subtitles box
 function record(){
+
+    // Get text, check if new
     let currentText = document.querySelectorAll('.vjs-text-track-display')[1].innerText.replace(/\n/g, ' ');
 
+    // Push to small list
     if(currentText != prevText){
         wordsList.push(currentText);
-        // console.log(wordsList);
     }
 
-    // currentText != prevText?
-    
-    // console.log(currentText):
-    // console.log('.');
 
     prevText = currentText;
 
+    // Combine small list into big list, refresh
     if(wordsList.length >= 5){
         masstext = wordsList.join(' ');
         console.log(masstext);
@@ -52,30 +50,22 @@ function record(){
 
 }
 
-
-
-
-
-
-// repeater
+// Check every 0.5s for new text
 var intervalId = window.setInterval(function(){
-    /// call your function here
+    /// 
     record();
   }, 500);
 
 
 
-// Btn functions
+// Saving functions
 
+// Convert mass array into text
 function massListJoin(){
     return(massList.join(' '));
 }
 
-
-
-
-
-
+// Create / Save Buttons
     (function () {
         var textFile = null,
           makeTextFile = function (text) {
@@ -93,14 +83,9 @@ function massListJoin(){
           };
         
         
-          var create = document.getElementById('create'),
-            textbox = document.getElementById('textbox');
+          var create = document.getElementById('create');
         
           create.addEventListener('click', function () {
-
-             // master text
-            // let value = massList.join(' ');
-
             var link = document.getElementById('downloadlink');
             link.href = makeTextFile(massListJoin());
             link.style.display = 'block';
